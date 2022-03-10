1855a8827184c78da02156d0b4be41821013ce87
Shivam Sharma <shivam.piyush99@gmail.com>
2022-03-10 11:09:19 +0530

    class using in list

---
+++ class_list/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Students stu1 = new Students()
+            {
+                name = "shiva",
+                age = 21,
+            }; 
+            Students stu2 = new Students()
+            {
+                name = "piyus",
+                age = 22,
+            };
+            List<Students> mystudent = new List<Students>();
+            mystudent.Add(stu1);
+            mystudent.Add(stu2);
+            foreach (Students x in mystudent)
+            {
+                Console.WriteLine("Name is {0} and age is {1}", x.name, x.age);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ class_list/Students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_list
+{
+    internal class Students
+    {
+        public string name { get; set; }
+        public int age  { get; set; }
+        
+    }
+}
